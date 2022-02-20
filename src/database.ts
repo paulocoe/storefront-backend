42d52620166c5ad5159c91d42e88edfb975f77bc
Paulo Coelho <paulo_coe@hotmail.com>
2022-02-20 22:26:50 +0000

    Adds database client

---
+++ src/database.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import { Pool } from "pg";
+
+dotenv.config();
+
+const client = new Pool({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+export default client;
