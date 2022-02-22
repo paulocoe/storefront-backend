88458e75dc8680ca3b49ae9482599a521dcaf74d
Paulo Coelho <paulo_coe@hotmail.com>
2022-02-22 09:35:04 +0000

    Adds show route

--- src/handlers/products.handler.ts
+++ src/handlers/products.handler.ts
@@ -4,10 +4,26 @@ import ProductValidator from "../validators/product.validator";
 
 const validator = new ProductValidator();
 const store = new ProductStore();
+const routePrefix = "/products";
 
 const index = async (_req: Request, res: Response) => {
   const products = await store.index();
   res.json(products);
+};
+
+const show = async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  if (!id) {
+    res.status(400).send("Invalid product id.");
+    return;
+  }
+
+  const product = await store.show(id);
+  if (!product) {
+    res.status(404).send("Product not found.");
+    return;
+  }
+  res.status(200).json(product);
 };
 
 const create = async (req: Request, res: Response) => {
@@ -25,8 +41,9 @@ const create = async (req: Request, res: Response) => {
 };
 
 const productsRoutes = (app: express.Application) => {
-  app.get("/products", index);
-  app.post("products/create", create);
+  app.get(`${routePrefix}/`, index);
+  app.get(`${routePrefix}/:id`, show);
+  app.post(`${routePrefix}/create`, create);
 };
 
 export default productsRoutes;
