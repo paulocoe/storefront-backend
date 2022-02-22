b59b38be5cd97193f0d25c05a9d6e481fafd4efc
Paulo Coelho <paulo_coe@hotmail.com>
2022-02-22 09:34:31 +0000

    Implements product handler

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,12 @@
 import express from "express";
+import productsRoutes from "./handlers/products.handler";
+import userRoutes from "./handlers/users.handler";
 const app = express();
+const port = "3000";
+const address = `0.0.0.0:${port}`;
+
+productsRoutes(app);
+
+app.listen(port, () => {
+  console.log(`Starting app on: ${address}`);
+});
